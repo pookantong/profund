04d49cc10df2ef3d8feb3783b54cb7fb099ce91d
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2022-08-15 14:00:23 +0700

    up

--- lab2_2nd.c
+++ lab2_2nd.c
@@ -6,18 +6,14 @@ int main()
     scanf("%d",&num);
     for(int i = 1;i <= num;i++)//จำนวนบรรทัด
     {
-        for(int j = num;j>=1;j--)//renderฝั่งซ้าย
+        for (int j = num; j > i; j--)//เว้นเป็นรูปสามเหลี่ยมเฉียงลง
         {
-            if(j>i)
             printf(" ");
-            else if(j<=i)
-            printf("#");
         }
-        for (int k = 1; k < i; k++)//renderฝั่งขวา
+        for (int k = 1; k < i*2; k++)//แสดงผลเพิ่มทีละ2ต่อบรรทัด
         {
             printf("#");
         }
-        
         printf("\n");
     }
     return 0;
