342ba7d03cc418b872f8fad7f230d0e963fcb90d
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2022-08-15 13:53:08 +0700

    up

---
+++ lab2_1st.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("INPUT YOUR NUMBER : ");
+    scanf("%d",&num);
+    for(int i = 1;i <= num;i++)
+    {
+        for(int j = num;j>=1;j--)
+        {
+            if(j>i)
+            printf(" ");
+            else if(j<=i)
+            printf("#");
+        }
+        for (int k = 1; k < i; k++)
+        {
+            printf("#");
+        }
+        
+        printf("\n");
+    }
+
+    
+
+}
