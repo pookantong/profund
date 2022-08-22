3bb26581d109e268587f8b280d703bff413682f6
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2022-08-22 13:19:54 +0700

    -

---
+++ lab3_2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("INPUT YOUR NUMBER : ");
+    scanf("%d",&num);
+    for(int i = num;i > 1;i--)//จำนวนบรรทัด
+    {
+        for (int j = num; j > i; j--)//เว้นเป็นรูปสามเหลี่ยมเฉียงลง
+        {
+            printf(" ");
+        }
+        for (int k = 1; k < i*2; k++)//แสดงผลเพิ่มทีละ2ต่อบรรทัด
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    //reverse code
+    for(int i = 1;i <= num;i++)//จำนวนบรรทัด
+    {
+        for (int j = num; j > i; j--)//เว้นเป็นรูปสามเหลี่ยมเฉียงขึ้น
+        {
+            printf(" ");
+        }
+        for (int k = 1; k < i*2; k++)//แสดงผลเพิ่มทีละ2ต่อบรรทัด
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    return 0;
+}
