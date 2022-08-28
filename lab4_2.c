10ee6e64bea0d7884f603260a77d7bcd3bc60a73
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2022-08-28 18:47:03 +0700

    -

--- lab4_2.c
+++ lab4_2.c
@@ -4,13 +4,13 @@ int main()
 {
     int count,count1;
     scanf("%d",&count);
-    count1=count*count;
+    count1=count*count;//จำนวนครั้งที่ต้องการกว้าง*สูง
     for (int i = 1; i <= count1; i++)
     {
         printf("*");
         if (i%count==0)
         {
-            printf("\n");
+            printf("\n");//เว้นบรรทัดเมื่อครบจำนวนใน1บรรทัดแล้ว
         }
     }
     
